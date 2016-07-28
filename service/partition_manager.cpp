460db0878ccfbb6663b208696fcc0634a0898400
Xu Shaohua <xushaohua@linuxdeepin.com>
2016-07-29 06:52:31 +0800

    service: Added PartitionManager
    Change-Id: Ib1957475d48b41e5773873244e18b94d34216ffc

--- service/partition_manager.cpp
+++ service/partition_manager.cpp
@@ -2,4 +2,158 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
-#include "partition_manager.h"
+#include "service/partition_manager.h"
+
+#include <parted/parted.h>
+#include <string.h>
+#include <QDebug>
+
+namespace service {
+
+namespace {
+
+const char kPartationTableGpt[] = "gpt";
+const char kPartationTableMsDos[] = "msdos";
+
+//// Flush linux kernel caches. Ensure coherency between the caches of the whole
+//// disk device and the partition devices.
+//bool FlushDevice(PedDevice* device) {
+//  bool success = false;
+//  if (ped_device_open(device)) {
+//    success = static_cast<bool>(ped_device_sync(device));
+//    ped_device_close(device);
+//  }
+//  return success;
+//}
+//
+//// Refers: http://stackoverflow.com/questions/14127210/
+//// After the kernel boots, `udevd` is used to create device nodes for
+//// all detected devices. That is a relatively time consuming task that
+//// has to be completed for the boot process to continue, otherwise
+//// there is a risk of services failing due to missing device nodes.
+//// `udevadm settle` waits for udevd to process the device creation events
+//// for all hardware devices, thus ensuring that any device nodes
+//// have been created successfully before proceeding.
+//void SettleDevice(int timeout) {
+//  const QString cmd = QString("udevadm settle --timeout=%1").arg(timeout);
+//  // TODO(xushaohua):
+//  Q_UNUSED(cmd);
+//}
+//
+//bool CommitDiskChanges(PedDisk* disk) {
+//  bool success = static_cast<bool>(ped_disk_commit_to_dev(disk));
+//  if (success) {
+//    success = static_cast<bool>(ped_disk_commit_to_os(disk));
+//  }
+//  return success;
+//}
+
+}  // namespace
+
+PartitionManager::PartitionManager(QObject* parent) : QObject(parent) {
+  this->setObjectName(QStringLiteral("partition_manager"));
+
+  this->initConnections();
+}
+
+void PartitionManager::initConnections() {
+  connect(this, &PartitionManager::refreshDevices,
+          this, &PartitionManager::doRefreshDevices);
+}
+
+void PartitionManager::doRefreshDevices() {
+// 1. List Devices
+// 1.1. Retrieve metadata of each device.
+//   * partition-table type
+//   * label
+//   * path
+//   * capacity
+//   * freespace
+// 2. List partitions of each device.
+// 3. Retrieve partition metadata.
+//   * partition label.
+//   * path
+//   * uuid
+//   * os-probe
+//   * filesystem type
+//   * freespace
+//   * total capacity
+//   * mounted ?
+//   * mount paths
+
+  // Let libparted detect all devices and construct device list.
+  ped_device_probe_all();
+
+  QList<Device> devices;
+
+  PedDevice* p_device = NULL;
+  while ((p_device = ped_device_get_next(p_device)) != NULL) {
+    PedDiskType* device_type = ped_disk_probe(p_device);
+    PedDisk* disk = NULL;
+    // TODO(xushaohua): Filters USB device.
+    if (device_type == NULL) {
+      // Raw disk found.
+      // TODO(xushaohua): Check proper partition type.
+      disk = ped_disk_new_fresh(p_device, ped_disk_type_get("gpt"));
+      device_type = ped_disk_probe(p_device);
+    } else if (QString(kPartationTableGpt) == device_type->name ||
+               QString(kPartationTableMsDos) == device_type->name) {
+      disk = ped_disk_new(p_device);
+    } else {
+      // Ignores other type of device.
+      qDebug() << "Ignores other type of device:" << device_type->name;
+      continue;
+    }
+    qDebug() << "path:" << p_device->path;
+    qDebug() << "model:" << p_device->model;
+    qDebug() << "read only:" << p_device->read_only;
+    qDebug() << "sector size:" << p_device->sector_size;
+    qDebug() << "partition type:" << device_type->name;
+    qDebug() << "sectors:" << p_device->bios_geom.sectors;
+    qDebug() << "length:" << p_device->length;
+
+    Device device;
+    device.model = p_device->model;
+    device.path = p_device->path;
+    device.table = device_type->name;
+    device.heads = p_device->bios_geom.heads;
+    device.sectors = p_device->bios_geom.sectors;
+    device.cylinders = p_device->bios_geom.cylinders;
+    device.sector_size = p_device->sector_size;
+
+    QList<Partition> partitions;
+    PedPartition* p_partition = NULL;
+    while ((p_partition = ped_disk_next_partition(disk, p_partition)) != NULL) {
+      Partition partition;
+      partition.fs = p_partition->fs_type->name;
+      //partition.freespace;
+      //partition.length = ;
+      partition.first_sector = p_partition->geom.start;
+      partition.total_sectors = p_partition->geom.length;
+      partition.last_sector = p_partition->geom.end;
+      partition.path = ped_partition_get_path(p_partition);
+      // Avoid reading additional filesystem information if there is no path.
+      if (!partition.path.isEmpty()) {
+        // Read uuid from /dev/disk/by-uuid/
+        //partition.uuid;
+
+        // Read label based on filesystem type
+        //partition.label;
+ 
+        // Read possible Operating System version by calling `os-prober`.
+
+        partition.name = ped_partition_get_name(p_partition);
+      }
+      //partition.flags = {};
+    }
+
+    device.partitions = partitions;
+    devices.append(device);
+
+    //ped_disk_print(disk);
+//    ped_device_destroy(p_device);
+//    ped_disk_destroy(disk);
+  }
+}
+
+}  // namespace service
--- service/partition_manager.h
+++ service/partition_manager.h
@@ -6,14 +6,57 @@
 #define DEEPIN_INSTALLER_REBORN_SERVICE_PARTITION_MANAGER_H
 
 #include <QObject>
+#include <QList>
 
 namespace service {
+
+struct Partition {
+  // File system
+  QString fs;  // file system type.
+  qint64 freespace;
+  qint64 length;
+  QString label;
+  QString uuid;
+  QStringList mounts;
+
+  // Partition
+  QString path;
+  qint64 first_sector;
+  qint64 last_sector;
+  qint64 total_sectors;
+  QString name;
+  QStringList flags;
+  QString os;
+};
+
+struct Device {
+  QString model;
+  QString path;
+  QString table; // partition table.
+  qint64 freespace;
+  qint64 length;  // total sectors / disk capacity.
+  int heads;
+  int sectors;
+  int cylinders;
+  qint64 sector_size;
+  QList<Partition> partitions;
+};
 
 class PartitionManager : public QObject {
   Q_OBJECT
 
  public:
   explicit PartitionManager(QObject* parent = nullptr);
+
+ signals:
+  void refreshDevices();
+  void devicesRefreshed();
+
+ private:
+  void initConnections();
+
+ private slots:
+  void doRefreshDevices();
 };
 
 }  // namespace service
