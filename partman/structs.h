98fcbee67b5859b2f7d49fdb058350eab20da856
Xu Shaohua <xushaohua@linuxdeepin.com>
2016-08-23 21:10:55 +0800

    partman: Added structs.h
    Change-Id: I07599d50395274ecd88595531669b2fb560903e8

---
+++ partman/structs.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2016 Deepin Ltd. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#ifndef DEEPIN_INSTALLER_REBORN_PARTMAN_STRUCTS_H
+#define DEEPIN_INSTALLER_REBORN_PARTMAN_STRUCTS_H
+
+namespace partman {
+
+// This header file defines commonly used types and struct type in partman
+// module.
+
+typedef long long Sector;
+typedef long long ByteValue;
+
+// Size units defined in bytes
+// See https://en.wikipedia.org/wiki/Kibibyte
+const ByteValue kKibiByte = 1024;
+const ByteValue kMebiByte = kKibiByte * kKibiByte;
+const ByteValue kGibiByte = kMebiByte * kKibiByte;
+const ByteValue kTebiByte = kGibiByte * kKibiByte;
+const ByteValue kPebiByte = kTebiByte * kKibiByte;
+const ByteValue kExbiByte = kPebiByte * kKibiByte;
+
+}  // namespace partman
+
+#endif  // DEEPIN_INSTALLER_REBORN_PARTMAN_STRUCTS_H
