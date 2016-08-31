6f86a34943d83c6461988dcf78c034b3f89678a6
Xu Shaohua <xushaohua@linuxdeepin.com>
2016-08-31 20:52:20 +0800

    partman::Partition: Added members
    Change-Id: Icf869aa46d5fd6cbd36ea87b37f58a9310a278f4

--- partman/partition.cpp
+++ partman/partition.cpp
@@ -6,4 +6,29 @@
 
 namespace partman {
 
+Partition::Partition() {
+  this->reset();
+}
+
+Partition::~Partition() {
+
+}
+
+void Partition::reset() {
+  device_path = "";
+  uuid = "";
+  name = "";
+  partition_number = -1;
+  whole_device = false;
+  type = PartitionType::Unallocated;
+  status = PartitionStatus::Real;
+  sector_start = -1;
+  sector_end = -1;
+  sectors_used = -1;
+  sectors_unused = -1;
+  sectors_unallocated = -1;
+  flags.clear();
+  logicals.clear();
+}
+
 }  // namespace partman
--- partman/partition.h
+++ partman/partition.h
@@ -5,11 +5,56 @@
 #ifndef DEEPIN_INSTALLER_REBORN_PARTMAN_PARTITION_H
 #define DEEPIN_INSTALLER_REBORN_PARTMAN_PARTITION_H
 
+#include <QStringList>
+
+#include "partman/structs.h"
+
 namespace partman {
+
+enum class PartitionType {
+  Primary,
+  Logical,
+  Extended,
+  Unallocated,
+};
+
+enum class PartitionStatus {
+  Real,
+  New,
+  Formatted,
+};
+
+enum class PartitionAlignment {
+  Cylinder,  // Align to nearest cylinder
+  MebiByte,  // Align to nearest mebibyte
+  Strict,  // Strict alignment, no rounding.
+};
 
 class Partition {
  public:
-  Partition() {}
+  Partition();
+  ~Partition();
+
+  void reset();
+
+  QString device_path;
+  QString uuid;
+  QString name;
+  int partition_number;
+  bool whole_device;
+  PartitionType type;
+  PartitionStatus status;
+
+  Sector sector_start;
+  Sector sector_end;
+  Sector sectors_used;
+  Sector sectors_unused;
+  Sector sectors_unallocated;
+  bool inside_extended;
+  bool busy;
+  QStringList flags;
+
+  QList<Partition> logicals;
 };
 
 }  // namespace partman
