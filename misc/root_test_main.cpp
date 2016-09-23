ee8207b7c7591e61c04ec28dba2e1c646ca375e0
Xu Shaohua <xushaohua@linuxdeepin.com>
2016-09-23 18:52:00 +0800

    Added root-unittest
    Change-Id: I10a592093300931955f4bb7fa704f54598277325

---
+++ misc/root_test_main.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2016 Deepin Ltd. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#include "third_party/googletest/googletest/include/gtest/gtest.h"
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ partman/operation_test.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2016 Deepin Ltd. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#include <parted/parted.h>
+
+#include "partman/operation.h"
+
+#include "third_party/googletest/googletest/include/gtest/gtest.h"
+
+namespace partman {
+namespace {
+
+TEST(OperationTest, ApplyToDisk) {
+  ped_device_probe_all();
+
+  Partition partition;
+  partition.sector_start = 305072128;
+  partition.sector_end = 367165439;
+  partition.fs = FsType::Ext4;
+  partition.path = "/dev/sdc2";
+  Operation format_operation(OperationType::Format, partition, partition);
+  EXPECT_TRUE(format_operation.applyToDisk());
+}
+
+}  // namespace
+}  // namespace partman
