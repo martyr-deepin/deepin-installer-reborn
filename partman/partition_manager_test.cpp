ebe94f19b1ca6a955cd0421db7f41a5d5cde937b
Xu Shaohua <xushaohua@linuxdeepin.com>
2016-09-29 10:52:02 +0800

    PartitionManager: Added test case
    Change-Id: Ie036b31adb7143a89aacbb86f46b2f1802722ebe

--- partman/partition_manager_test.cpp
+++ partman/partition_manager_test.cpp
@@ -2,3 +2,17 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
+#include "partman/partition_manager.h"
+
+#include "third_party/googletest/googletest/include/gtest/gtest.h"
+
+namespace partman {
+namespace {
+
+TEST(PartitionManagerTest, ScanDevices) {
+  const DeviceList devices = ScanDevices();
+  EXPECT_FALSE(devices.isEmpty());
+}
+
+}  // namespace
+}  // namespace partman
