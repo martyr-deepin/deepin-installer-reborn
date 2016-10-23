71d6a8beaf7810ae4f53ce7c165fdd06505d88b3
Xu Shaohua <xushaohua@linuxdeepin.com>
2016-10-23 20:56:44 +0800

    Parse xrandr output
    Change-Id: I1e7aed07bcde0cffcaa0fbbc0bfcb18803e13d7c

--- misc/xrandr_switchy.cpp
+++ misc/xrandr_switchy.cpp
@@ -4,8 +4,10 @@
 
 #include <QDebug>
 #include <QList>
+#include <QRegularExpression>
 
 #include "base/command.h"
+#include "base/string_util.h"
 
 namespace {
 
@@ -24,6 +26,15 @@ struct Screen {
   int max_height;
 };
 typedef QList<Screen> ScreenList;
+QDebug& operator<<(QDebug& debug, const Screen& screen) {
+  debug << "Screen {"
+        << screen.id
+        << screen.min_width << "x" << screen.min_height
+        << screen.curr_width << "x" << screen.curr_height
+        << screen.max_width << "x" << screen.max_height
+        << "}";
+  return debug;
+}
 
 // Output mode.
 struct Mode {
@@ -36,6 +47,15 @@ struct Mode {
   bool is_preferred;  // Preferred mode
 };
 typedef QList<Mode> ModeList;
+QDebug& operator<<(QDebug& debug, const Mode& mode) {
+  debug << "Mode {"
+        << mode.name
+        << mode.width << "x" << mode.height
+        << (mode.is_used ? "used" : " ")
+        << (mode.is_preferred ? "preferred" : " ")
+        << "}";
+  return debug;
+}
 
 struct Output {
  public:
@@ -51,6 +71,20 @@ struct Output {
   ModeList modes;
 };
 typedef QList<Output> OutputList;
+QDebug& operator<<(QDebug& debug, const Output& output) {
+  debug << "Output {"
+        << output.name
+        << (output.is_connected ? "connected" : "disconnected")
+        << (output.is_primary ? "primary" : " ")
+        << output.width << "x" << output.height
+        << output.x << "x" << output.y
+        << "\n";
+  for (const Mode& mode : output.modes) {
+    debug << "  " << mode << "\n";
+  }
+  debug << "}";
+  return debug;
+}
 
 // `xrandr` result.
 struct XRandR {
@@ -59,23 +93,121 @@ struct XRandR {
   ScreenList screens;
   OutputList outputs;
 };
-
-Screen ParseScreen(const QString& text) {
-  Q_UNUSED(text);
-  Screen screen;
-  return screen;
+QDebug& operator<<(QDebug& debug, const XRandR& xrandr) {
+  for (const Screen& screen : xrandr.screens) {
+    debug << screen << "\n";
+  }
+  for (const Output& output : xrandr.outputs) {
+    debug << output << "\n";
+  }
+  return debug;
 }
 
-XRandR ParseXRandR(const QString& text) {
-  Q_UNUSED(text);
-  XRandR xrandr;
-  for (const QString& line : text.split('\n')) {
-    if (line.startsWith("Screen")) {
-      Screen screen = ParseScreen(line);
-      xrandr.screens.append(screen);
+// Parse Screen 0: minimum 320 x 200, current 1920 x 1080, maximum 8192 x 8192
+bool ParseScreen(const QString& text, Screen& screen) {
+  QRegularExpression reg("Screen (\\d+): minimum (\\d+) x (\\d+), "
+                         "current (\\d+) x (\\d+), maximum (\\d+) x (\\d+)");
+
+  QRegularExpressionMatch match = reg.match(text);
+  if (match.hasMatch() && (match.lastCapturedIndex() == 7)) {
+    screen.id = match.captured(1).toInt();
+    screen.min_width = match.captured(2).toInt();
+    screen.min_height = match.captured(3).toInt();
+    screen.curr_width = match.captured(4).toInt();
+    screen.curr_height = match.captured(5).toInt();
+    screen.max_width = match.captured(6).toInt();
+    screen.max_height = match.captured(7).toInt();
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Parses    1920x1080     60.00 +  59.93*   40.00
+bool ParseMode(const QString& text, Mode& mode) {
+  if (text.contains("+")) {
+    mode.is_preferred = true;
+  }
+  if (text.contains("*")) {
+    mode.is_used = true;
+  }
+  const QStringList items = text.trimmed().split(' ');
+  if (items.length() > 1) {
+    mode.name = items[0];
+    const QStringList values = items[0].split('x');
+    if (values.length() == 2) {
+      mode.width = values[0].toInt();
+      mode.height = values[1].toInt();
+      return true;
     }
   }
-  return xrandr;
+
+  return false;
+}
+
+// Parse eDP-1 connected primary 1920x1080+0+0 (normal left inverted right x axis y axis) 282mm x 165mm
+
+bool ParseOutput(const QString& text, Output& output) {
+  bool ok = false;
+  const QStringList& items = text.split(' ');
+  if (items.length() > 2) {
+    output.name = items[0];
+    output.is_connected = (items[1] == "connected");
+    QString geom;
+    if (items[2] == "primary") {
+      geom = items[3];
+      output.is_primary = true;
+    } else {
+      geom = items[2];
+    }
+    if (output.is_connected) {
+      // Matches "1920x1080+0+0"
+      QRegularExpression reg("(\\d+)x(\\d+)[^\\d](\\d+)[^\\d](\\d+)");
+      QRegularExpressionMatch match = reg.match(geom);
+
+      if (match.hasMatch() && (match.lastCapturedIndex() == 4)) {
+        output.width = match.captured(1).toInt();
+        output.height = match.captured(2).toInt();
+        output.x = match.captured(3).toInt();
+        output.y = match.captured(4).toInt();
+        ok = true;
+      }
+    } else {
+      ok = true;
+    }
+  }
+  return ok;
+}
+
+bool ParseXRandR(const QString& text, XRandR& xrandr) {
+  for (const QString& line : text.split('\n')) {
+    if (line.isEmpty()) {
+      // Do nothing.
+    } else if (line.startsWith("Screen")) {
+      Screen screen;
+      if (ParseScreen(line, screen)) {
+        xrandr.screens.append(screen);
+      } else {
+        qCritical() << "Parse screen failed:" << line;
+      }
+    } else if (line.startsWith(' ')) {
+      Mode mode;
+      Output& output = xrandr.outputs.last();
+      if (ParseMode(line, mode)) {
+        output.modes.append(mode);
+      } else {
+        qCritical() << "Parse mode failed:" << line;
+      }
+    } else {
+      Output output;
+      if (ParseOutput(line, output)) {
+        xrandr.outputs.append(output);
+      } else {
+        qCritical() << "Parse output failed:" << line;
+      }
+    }
+  }
+  return true;
 }
 
 // Switch to next mode
@@ -83,7 +215,7 @@ void SwitchMode(const XRandR& xrandr) {
   Q_UNUSED(xrandr);
 }
 
-}
+}  // namespace
 
 int main(void) {
   QString out, err;
@@ -97,7 +229,12 @@ int main(void) {
     return 1;
   }
 
-  XRandR xrandr = ParseXRandR(out);
+  XRandR xrandr;
+  if (!ParseXRandR(out, xrandr)) {
+    qCritical() << "Parse XRandR failed!";
+    return 1;
+  }
+  qDebug() << "xrandr:" << xrandr;
 
   SwitchMode(xrandr);
 
