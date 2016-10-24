aa9fc95de15ff695061433d74049985e2e33a22b
Xu Shaohua <xushaohua@linuxdeepin.com>
2016-10-24 10:44:00 +0800

    Added screen-info
    Change-Id: If5429a5cb4883020ef70939a6c3d3fdfaa6d935c

--- misc/screen_info.cpp
+++ misc/screen_info.cpp
@@ -2,3 +2,33 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
+#include <QApplication>
+#include <QDebug>
+#include <QDesktopWidget>
+
+static void PrintScreen(QDesktopWidget* desktop_widget) {
+  const int screen_count = desktop_widget->screenCount();
+  for (int i = 0; i < screen_count; i++) {
+    const QRect geom = desktop_widget->screenGeometry(i);
+    qDebug() << "Screen: " << i << geom;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  QApplication app(argc, argv);
+
+  QDesktopWidget* desktop_widget = QApplication::desktop();
+  QObject::connect(desktop_widget, &QDesktopWidget::primaryScreenChanged,
+      [&]() {
+        qDebug() << "primary screen changed:"
+                 << desktop_widget->primaryScreen();
+        PrintScreen(desktop_widget);
+      });
+  QObject::connect(desktop_widget, &QDesktopWidget::screenCountChanged,
+      [&]() {
+        qDebug() << "screen count changed:" << desktop_widget->screenCount();
+        PrintScreen(desktop_widget);
+      });
+
+  return app.exec();
+}
