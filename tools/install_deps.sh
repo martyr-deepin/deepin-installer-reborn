f1888e51d9c562b61d872bc021d0d568a20bb498
Xu Shaohua <xushaohua@linuxdeepin.com>
2017-01-09 10:44:20 +0800

    tools: Added install_deps.sh
    Change-Id: I23e4bac28666564a3445b914dc8599b81ff4a886

---
+++ tools/install_deps.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Copyright (c) 2016 Deepin Ltd. All rights reserved.
+# Use of this source is governed by General Public License that can be found
+# in the LICENSE file.
+
+# Install package dependencies for development environment.
+
+sudo apt install \
+    cmake \
+    g++ \
+    qt5-qmake \
+    qt5-default \
+    qtbase5-dev \
+    pkg-config \
+    libattr1-dev \
+    liblz4-dev \
+    liblzma-dev \
+    liblzo2-dev \
+    libparted-dev \
+    libqt5x11extras5-dev \
+    libx11-dev \
+    libxext-dev \
+    libxrandr-dev \
+    libxss-dev \
+    libxtst-dev \
+    zlib1g-dev \
+    aircrack-ng \
+    btrfs-progs \
+    dosfstools \
+    e2fsprogs \
+    hfsprogs \
+    jfsutils \
+    mtools \
+    ntfs-3g \
+    os-prober \
+    squashfs-tools \
+    tshark \
+    udev \
+    util-linux \
+    x11-xserver-utils \
+    xkb-data
