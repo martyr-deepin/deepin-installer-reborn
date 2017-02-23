bff82f19ab218fa4704aa584ae82199e59020b7b
Xu Shaohua <xushaohua@linuxdeepin.com>
2017-02-23 09:54:02 +0800

    tools: Added sync_repo.sh
    Change-Id: I493c6e209af87ed02dd4039d8bd51e2a530d0447

---
+++ tools/sync_repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright (c) 2016 Deepin Ltd. All rights reserved.
+# Use of this source is governed by General Public License that can be found
+# in the LICENSE file.
+
+# Sync project repo and build.
+
+rsync -ave ssh demo@ip:/tmp/deepin-installer-reborn/ installer \
+  --exclude cmake-build-debug && \
+  mkdir -p installer/build && \
+  cd installer/build && \
+  cmake -DCMAKE_INSTALL_PREFIX=/usr .. && \
+  make -j4 deepin-installer-reborn && \
+  sudo install deepin-installer-reborn /usr/bin/ && \
+  sudo cp -rf i18n/ /usr/share/deepin-installer-reborn/ && \
+  sudo systemctl restart lightdm
