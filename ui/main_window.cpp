df6b0fcdb1451b3b888f1efb199ac30f25cd5658
Xu Shaohua <xushaohua@linuxdeepin.com>
2017-03-28 08:37:11 +0800

    MainWindow: Reset first page
    Change-Id: I2d38ff066bf0c69e9dbf2f426a27782020177e0d

--- ui/main_window.cpp
+++ ui/main_window.cpp
@@ -69,7 +69,6 @@ void MainWindow::fullscreen() {
 
   multi_head_manager_->updateWallpaper();
   ShowFullscreen(this);
-  current_page_ = PageId::SystemInfoId;
   this->goNextPage();
 
   if (auto_install_) {
