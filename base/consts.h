a06de35571d9a02d6403c1494cdeb2104474fc5e
xushaohua <xushaohua@deepin.com>
2017-11-22 13:50:31 +0800

    Release 2.2.9
    Change-Id: I0c061f56f33996053d236ca954a9a290af3cf842

--- base/consts.h
+++ base/consts.h
@@ -20,7 +20,7 @@
 
 namespace installer {
 
-const char kAppVersion[] = "2.2.8";
+const char kAppVersion[] = "2.2.9";
 
 // Default locale used in program.
 const char kDefaultLang[] = "en_US.UTF-8";
