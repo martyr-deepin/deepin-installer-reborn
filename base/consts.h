3ac227a9b98d3af088f09a13098d7fdda2590f83
xushaohua <xushaohua@deepin.com>
2018-01-25 14:32:53 +0800

    Release 2.2.14
    Change-Id: Ib7fc036250dc63f3f4b356691ce57116b2534d45

--- base/consts.h
+++ base/consts.h
@@ -20,7 +20,7 @@
 
 namespace installer {
 
-const char kAppVersion[] = "2.2.13";
+const char kAppVersion[] = "2.2.14";
 
 // Default locale used in program.
 const char kDefaultLang[] = "en_US.UTF-8";
