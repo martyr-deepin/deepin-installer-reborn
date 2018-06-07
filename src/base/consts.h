b2fb01e65e731c5daf4455c415416b1ef8799681
Xu Shaohua <xushaohua2016@outlook.com>
2018-06-07 14:45:12 +0800

    Release 2.2.17
    Change-Id: Icc7f0a568188f83edd1bc9f51cb6c93568929802

--- src/base/consts.h
+++ src/base/consts.h
@@ -20,7 +20,7 @@
 
 namespace installer {
 
-const char kAppVersion[] = "2.2.16";
+const char kAppVersion[] = "2.2.17";
 
 // Default locale used in program.
 const char kDefaultLang[] = "en_US.UTF-8";
