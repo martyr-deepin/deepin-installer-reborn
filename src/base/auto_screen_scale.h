9cf570b97f3644a335888a9ef5ac769a464982d6
justforlxz <justforlxz@gmail.com>
2019-01-29 11:29:31 +0800

    refactor(AutoScale): use XRRGetScreenResourcesCurrent to replace XRRGetScreenResources function
    XRRGetScreenResource does not work on some old graphics cards
    
    Change-Id: Ib7e51a96549bcb61f2f2c414770685dda0ae6480

--- src/base/auto_screen_scale.h
+++ src/base/auto_screen_scale.h
@@ -8,43 +8,50 @@ namespace Utils
 {
 void AutoScreenScale()
 {
+    double scaleRatio = 1.0;
     Display *display = XOpenDisplay(NULL);
 
-    XRRScreenResources *resources = XRRGetScreenResources(display, DefaultRootWindow(display));
-    double scaleRatio = 1.0;
-    std::list<double> scaleRatioList;
+    XRRScreenResources *resources = XRRGetScreenResourcesCurrent(display, DefaultRootWindow(display));
 
-    for (int i = 0; i < resources->noutput; i++)
-    {
-        XRROutputInfo *outputInfo = XRRGetOutputInfo(display, resources, resources->outputs[i]);
-        if (outputInfo->crtc == 0 || outputInfo->mm_width == 0)
-            continue;
-
-        XRRCrtcInfo *crtInfo = XRRGetCrtcInfo(display, resources, outputInfo->crtc);
-        if (crtInfo == nullptr)
-            continue;
-
-        scaleRatio = static_cast<double>(crtInfo->width) / static_cast<double>(outputInfo->mm_width) / (1366.0 / 310.0);
-
-        // When you have multiple screens,
-        // if one of the resolutions is very large,
-        // it will cause the copy mode to display abnormally.
-        if (scaleRatio > 1 + 2.0 / 3.0) {
-            scaleRatioList.push_back(2.0);
-        }
-        else if (scaleRatio > 1 + 1.0 / 3.0) {
-            scaleRatioList.push_back(1.5);
-        }
-        else {
-            scaleRatioList.push_back(1.0);
-        }
+    if (!resources) {
+        resources = XRRGetScreenResources(display, DefaultRootWindow(display));
+        qWarning() << "get XRRGetScreenResourcesCurrent failed, use XRRGetScreenResources.";
     }
 
-    XRRFreeScreenResources(resources);
+    if (resources) {
+        std::list<double> scaleRatioList;
+        for (int i = 0; i < resources->noutput; i++)
+        {
+            XRROutputInfo *outputInfo = XRRGetOutputInfo(display, resources, resources->outputs[i]);
+            if (outputInfo->crtc == 0 || outputInfo->mm_width == 0)
+                continue;
 
-    if (!scaleRatioList.empty()) {
-        scaleRatioList.sort();
-        scaleRatio = *scaleRatioList.begin();
+            XRRCrtcInfo *crtInfo = XRRGetCrtcInfo(display, resources, outputInfo->crtc);
+            if (crtInfo == nullptr)
+                continue;
+
+            scaleRatio = static_cast<double>(crtInfo->width) / static_cast<double>(outputInfo->mm_width) / (1366.0 / 310.0);
+
+            // When you have multiple screens,
+            // if one of the resolutions is very large,
+            // it will cause the copy mode to display abnormally.
+            if (scaleRatio > 1 + 2.0 / 3.0) {
+                scaleRatioList.push_back(2.0);
+            }
+            else if (scaleRatio > 1 + 1.0 / 3.0) {
+                scaleRatioList.push_back(1.5);
+            }
+            else {
+                scaleRatioList.push_back(1.0);
+            }
+        }
+
+        if (!scaleRatioList.empty()) {
+            scaleRatioList.sort();
+            scaleRatio = *scaleRatioList.begin();
+        }
+
+        XRRFreeScreenResources(resources);
     }
 
     qDebug() << "scale factor: " << QString::number(scaleRatio);
